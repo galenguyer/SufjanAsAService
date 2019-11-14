846ad2a7206e8d6b04a4172fc48597b0e7e0b84d
Galen G <github@mastrchef.rocks>
2019-11-13 19:44:09 -0500

    Create SaaS project

---
+++ SufjanAsAService/Controllers/SufjanController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace SufjanAsAService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SufjanController : ControllerBase
+    {
+        private readonly ILogger<SufjanController> _logger;
+
+        public SufjanController(ILogger<SufjanController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Hi!";
+        }
+    }
+}
