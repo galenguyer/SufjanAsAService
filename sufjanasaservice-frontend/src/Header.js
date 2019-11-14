b7ec9c53c47632868bede0b082766372a49f80de
Galen G <github@mastrchef.rocks>
2019-11-13 23:37:15 -0500

    Very basic react components

---
+++ sufjanasaservice-frontend/src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header>
+            <h1>Sufjo Is Saddo</h1>
+            <h5>A Sad Song by Sufjan (as a Service) Stevens</h5>
+        </header>
+    )
+}
+
+export default Header
---
+++ sufjanasaservice-frontend/src/Lyrics.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Lyrics() {
+  return (
+    <div className="Lyrics">
+        <p>These are song lyrics</p>
+    </div>
+  );
+}
+
+export default Lyrics;
